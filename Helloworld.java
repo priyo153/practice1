079a18b217da8f72a63f4db08e681275eb7dab3b
priyo153 <38110137+priyo153@users.noreply.github.com>
2019-01-08 23:36:27 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -5,6 +5,7 @@ public static void main(String [] args){
     System.out.println("hello");
     System.out.println("everyone");
     System.out.println("im practicing");
+    System.out.println("for my job");
   }
 }
 
